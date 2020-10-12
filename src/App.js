ad2cb8b67812f8c047b4463d39112e39dbdcfdb8
MarijaCvetkovicc <jmcvetkovic@gmail.com>
2020-10-12 18:42:53 +0200

    added

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component{
+class App extends Component {
 
-  state={
-    todoList:[]    
+  state = {
+    todoList: []
   }
-  deleteTodoTask = (event, index) =>{
-    var taskArray=[...this.state.todoList];
-    taskArray.splice(index,1);
-    this.setState({todoList:taskArray});
+  deleteTodoTask = (event, index) => {
+    var taskArray = [...this.state.todoList];
+    taskArray.splice(index, 1);
+    this.setState({ todoList: taskArray });
   }
   AddTodoTask = (event) => {
     var taskDesc = event.target.elements.todoTask.value;
-    if(taskDesc.length > 0){
+    if (taskDesc.length > 0) {
       this.setState({
-        todoList:[...this.state.todoList, taskDesc]
+        todoList: [...this.state.todoList, taskDesc]
       });
       event.target.reset();
 
     }
     event.preventDefault();
   }
-  render(){
-  return (
-    <div>
-      <div className="jumbotron jumbotron-fluid py-2">
-      <div className="container">
-      <h1 className="display-4">List of Todos</h1>
-      </div>
-      </div>
-  <form className="mb-3" onSubmit={this.AddTodoTask}> 
-  <div className="input-group">
-    <input type="text" name="todoTask" className="form-control" placeholder="Enter text here..." autoComplete="off"/>
-    <div className="input-group-append">
-      <button type="submit" className="btn btn-outline-success">Add</button>
-    </div>
-    </div>
-  </form>
+  render() {
+    return (
+      <div>
+        <div className="jumbotron jumbotron-fluid py-2">
+          <div className="container">
+            <h1 className="display-4">List of Todos</h1>
+          </div>
+        </div>
+        <form className="mb-3" onSubmit={this.AddTodoTask}>
+          <div className="input-group">
+            <input type="text" name="todoTask" className="form-control" placeholder="Enter text here..." autoComplete="off" />
+            <div className="input-group-append">
+              <button type="submit" className="btn btn-outline-success">Add</button>
+            </div>
+          </div>
+        </form>
 
-    <ul className="list-group">
-    {
-      this.state.todoList.map(
-        (item,index) => {
-          return <li className="list-group-item" key={index}>
-            {item}
-            <button className="btn btn-sm btn-outline-danger float-right" onClick={(event)=>{this.deleteTodoTask(event,index)}}>X</button>
-          </li>
-        }
-      )
-    }
-    </ul>
+        <ul className="list-group">
+          {
+            this.state.todoList.map(
+              (item, index) => {
+                return <li className="list-group-item" key={index}>
+                  {item}
+                  <button className="btn btn-sm btn-outline-danger float-right" onClick={(event) => { this.deleteTodoTask(event, index) }}>X</button>
+                </li>
+              }
+            )
+          }
+        </ul>
 
-  </div>
-  );
+      </div>
+    );
   }
 
 }

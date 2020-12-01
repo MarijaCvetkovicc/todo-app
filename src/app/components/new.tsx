df87e95e9089e65632e7de0a24c33d07ee937445
MarijaCvetkovicc <jmcvetkovic@gmail.com>
2020-12-01 19:39:11 +0100

    new file

---
+++ src/app/components/new.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class New extends Component {
+
+    render() {
+      return (
+       <div>
+           HELLO HERE!
+       </div>
+      );
+    }
+  }
+  export default New;

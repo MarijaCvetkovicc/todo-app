def85d64a7d878ad471a6988db601b3554acafe2
MarijaCvetkovicc <jmcvetkovic@gmail.com>
2020-12-02 21:09:20 +0100

    here

--- src/app/components/TodoEdit/TodoEdit.tsx
+++ src/app/components/TodoEdit/TodoEdit.tsx
@@ -9,9 +9,7 @@ interface TodoEditProps extends RouteComponentProps<{ id: string }> {
 interface TodoEditState {
     item: ITodoItem,
 }
-
 class TodoEdit extends React.Component<TodoEditProps, TodoEditState>{
-
     constructor(props: TodoEditProps) {
         super(props);
         this.state = {
